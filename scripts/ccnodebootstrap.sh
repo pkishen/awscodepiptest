15d3c412b45a7cd2016c5190393ddfff7a8e9c71
Parthu K <krisparthu@gmail.com>
2016-07-04 14:35:53 -0700

    Create ccnodebootstrap.sh

---
+++ scripts/ccnodebootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#keyName=$1
+#componentBaseUrl=$2
+#nodename=$3 
+#hostname=$4
+#vmUserName=$5
+#complianceadminusername=$6
+#env=$7
+#component=$8
+#orchestratorbaseurl:$9
+URL=http://$9:33001/keys/$8/$6/envs/$7/nodes
+PUBLICKEY=curl -X POST -d
+{"keyName":"$1", "componentBaseUrl":"$2", "nodename":"$3", "hostname": "$4", "vmUserName": "$5" }
+$URL
+echo $PUBLICKEY >> /home/$5/.ssh/id_rsa.pub
+cp /home/$5/.ssh/id_rsa.pub /home/$5/.ssh/authorized_keys
