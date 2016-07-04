e1cc74377130a61266c353de7f73ab3777de7265
Parthu K <krisparthu@gmail.com>
2016-07-04 15:13:53 -0700

    Update testscriptDocker.sh

--- scripts/testscriptDocker.sh
+++ scripts/testscriptDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir ~/code
-git clone https://github.com/sysgain/km-docker-nodejs.git /home/pkishen/code
-cd ~/code
+mkdir ~/km-docker-nodejs
+git clone https://github.com/sysgain/km-docker-nodejs.git /home/pkishen/km-docker-nodejs
+cd ~/km-docker-nodejs
 COMPOSE_HTTP_TIMEOUT=180 sudo docker-compose up
 sudo docker-compose up
